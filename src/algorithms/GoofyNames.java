537d701c81585ad1d0ab047a682b78d703ab698f
league <league@wts-imac-11.attlocal.net>
2018-09-28 18:25:12 -0700

    EvilPolygons, GoofyNames, Guessing Pi ip

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		// String goofyName = "";
+		String goofyName = "";
 
 		// 1. Ask the user to enter their name
 		String name = JOptionPane.showInputDialog("What is your Name?");
@@ -24,17 +24,15 @@ public class GoofyNames {
 			String letters = name.substring(a, a + 1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			// and lower case for ODD characters.
-			 int intname = Integer.parseInt(name);
-			 if(intname%intname == 0) {
-			 System.out.println(name.toUpperCase());
-			 }
-			 else {
-			 System.out.println(name.toLowerCase());
-			 }
+			if (a % 2 == 0) {
+				System.out.println(name.toUpperCase());
+			} else {
+				System.out.println(name.toLowerCase());
+			}
 			// 6. ADD this String (containing 1 char) to the goofyName String
-			String newname = name + intname;
+			goofyName = goofyName + letters;
 		}
 		// 7. Use pop-up to show user their Goofy name
-		// JOptionPane.showMessageDialog(null, newname);
+		JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -1,4 +1,7 @@
+//COLORCHOICE = (1 = 0), (2 = 1), (3 = 2)
+
 package extra;
+
 import java.awt.Color;
 
 import javax.swing.JOptionPane;
@@ -6,24 +9,41 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
-		
-		//1. Create a new Robot
 
-		//2. Set the speed to 100
+		// 1. Create a new Robot
+		Robot apple = new Robot();
+		// 2. Set the speed to 100
+		apple.setSpeed(100);
+		int colorChoice = JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE,
+				JOptionPane.DEFAULT_OPTION, null, new String[] { "Red", "Green", "Blue" }, 0);
+		System.out.println(colorChoice);
+		// 3. Use if statements to check the the value of colorChoice and set the pen
+		// color accordingly
+		if (colorChoice == 0) {
+			apple.setPenColor(Color.RED);
+		} else if (colorChoice == 1) {
+			apple.setPenColor(Color.GREEN);
+		} else if (colorChoice == 2) {
+			apple.setPenColor(Color.RED);
+		}
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+		// 4. Ask the use how many polygons they want to be drawn.
+		String poly = JOptionPane.showInputDialog("How many polygons do you want the robot to draw??");
+		// 5. Use the robot to draw the number of polygons the user requested.
+		int gon = Integer.parseInt(poly);
 
-		//7. Challenge: add more colors to the Option Dialog.
+		apple.penDown();
+		
+		for (int a = 0; a < gon; a++) {
+			for (int b = 0; b < 3; b++) {
+				apple.move(15);
+				apple.turn(60);
+			}
+		}
+		// 6. Make it so your shapes do not overlap
+
+		// 7. Challenge: add more colors to the Option Dialog.
 	}
 }
-
